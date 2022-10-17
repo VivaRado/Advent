750a330b43ed579a7f68a8bd8e96a5f480b33422
root <root@debian>
2022-10-17 22:34:48 +0300

    gfonts_prep

--- sources/fix_scripts/fix2.py
+++ sources/fix_scripts/fix2.py
@@ -1,15 +1,15 @@
 from defcon import Font
 
 from glob import glob
-
-
+import os
+import shutil
 
 fonts = glob("advent_pro/*.ufo")
 
 for font in fonts:
     f = Font(font)
-    f.info.copyright = "Copyright 2011 The Advent Pro Project Authors (https://github.com/VivaRado/Advent)"
+    f.info.copyright = "Copyright (c) VivaRado, Andreas Kalpakidis (support@vivarado.com) and Advent Pro Project Authors to the extent of their contribution (https://github.com/VivaRado/Advent)"
     f.info.openTypeNameLicense = "This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL"
     f.info.openTypeNameLicenseURL = "https://scripts.sil.org/OFL"
-    f.save()
-    
+    f.info.openTypeNameVersion = "Version 2.0"
+    f.info.openTypeOS2Type = [0]
