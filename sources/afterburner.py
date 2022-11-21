10e5f4a813e7dff08be0cd0a39c5b08634da9ac7
Yanone <post@yanone.de>
2022-11-21 14:01:54 +0100

    Adjusted afterburner to accommodate italic settings

--- sources/afterburner.py
+++ sources/afterburner.py
@@ -1,8 +1,11 @@
 import sys
+import math
 from fontTools.ttLib import TTFont
 
 font = TTFont(sys.argv[-1])
 name = font["name"]
+
+stat = font["STAT"]
 
 
 def set_name(nameID, string):
@@ -23,6 +26,21 @@ def get_name(nameID):
             return record.toUnicode()
 
 
+def get_axis(tag):
+    for i, axis in enumerate(font["STAT"].table.DesignAxisRecord.Axis):
+        if axis.AxisTag == tag:
+            return i, axis
+
+
+def get_axis_value(axisTag, name):
+    for axisValue in stat.table.AxisValueArray.AxisValue:
+        if (
+            axisValue.AxisIndex == get_axis(axisTag)[0]
+            and get_name(axisValue.ValueNameID) == name
+        ):
+            return axisValue
+
+
 set_name(2, "Italic")
 name.setName("Regular", 262, 3, 1, 1033)
 name.setName("Regular", 262, 1, 0, 0)
@@ -30,7 +48,30 @@ name.setName("Regular", 262, 1, 0, 0)
 for nameID in (3, 4, 6):
     set_name(nameID, get_name(nameID).replace("Regular", "Italic"))
 
-stat = font["STAT"]
-stat.table.AxisValueArray.AxisValue[3].ValueNameID = 262
 
+get_axis_value("wght", "Italic").ValueNameID = 262
+
+# Italic angle
+post = font["post"]
+post.italicAngle = -12
+font["post"] = post
+
+# Caret slope
+hhea = font["hhea"]
+hhea.caretSlopeRise = font["head"].unitsPerEm
+hhea.caretSlopeRun = round(
+    math.atan(math.radians(abs(post.italicAngle))) * font["head"].unitsPerEm
+)
+font["hhea"] = hhea
+
+font["head"].macStyle |= 1 << 1
+
+# Unset
+for value in [0, 5, 6]:
+    font["OS/2"].fsSelection &= ~(1 << value)
+# Set Italic
+font["OS/2"].fsSelection |= 1 << 0
+
+
+font["STAT"] = stat
 font.save(sys.argv[-1])
